01fc087528e302be369057dadc3fe4d13ea2b3f5
Svastik Sharma <svastiksharma13@gmail.com>
2024-03-04 23:04:34 -0500

    update

--- src/test/kotlin/org/example/Test_one.kt
+++ src/test/kotlin/org/example/Test_one.kt
@@ -1,5 +1,8 @@
 package org.example
 
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.testng.Assert
 import org.testng.annotations.Test
 
 class TestOne{
@@ -7,7 +10,17 @@ class TestOne{
     @Test
     fun openWebpage(){
 
+        System.setProperty("webdriver.chrome.driver", "src/main/kotlin/org/example/drivers/chrome.exe")
 
+        val driver: WebDriver = ChromeDriver() //immutable, give driver a type so code is readable
+
+        val url: String = "http://www.google.com"
+
+        driver.get(url); //open web page
+
+        Assert.assertEquals(driver.currentUrl, url, "Url does not match!")
+
+        driver.quit() // shut down driver
 
     }
 
